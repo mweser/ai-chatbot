92d29c018c14b815f6b3782e65e03341a6d7cf00
Matt Wood <matt@mattwood.co>
2024-06-29 21:02:52 -0700

    complete

--- lib/rag.ts
+++ lib/rag.ts
@@ -1,21 +1,16 @@
 import axios from 'axios'
+import { config } from 'dotenv'
+import FormData from 'form-data'
 import * as fs from 'fs'
 import * as path from 'path'
-import FormData from 'form-data'
+config()
 
 async function uploadFile() {
   const url = 'https://api.unstructuredapp.io/general/v0/general'
-  const apiKey = 'oWDV3tiWSrz7BybnWGNlgCeBWfRLZW'
-
-  // Create a new FormData instance
+  const apiKey = process.env.UNSTRUCTURED_API_KEY
   const formData = new FormData()
-
-  // Append the file to the FormData
-  formData.append('files', fs.createReadStream('public/excerpt.pdf'))
-
-  // Add configuration options
+  formData.append('files', fs.createReadStream('public/excerpt2.pdf'))
   formData.append('include_page_breaks', 'false')
-  formData.append('include_image_data', 'false')
 
   try {
     console.log('Sending request...')
@@ -31,7 +26,6 @@ async function uploadFile() {
     console.log('Status:', response.status)
     console.log('Status Text:', response.statusText)
 
-    // Filter the response data
     const filteredData = response.data
       .filter((item: any) => {
         return item.type === 'NarrativeText' && !/^\d+$/.test(item.text.trim())
@@ -43,24 +37,18 @@ async function uploadFile() {
         }
       }))
 
-    // Create a versioned filename
     const version = '1' // Increment this for new versions
-    const configCode = 'hrt' // h: hi_res, r: raw text, t: no tables
-    const timestamp = new Date()
-      .toISOString()
-      .replace(/[-:]/g, '')
-      .split('T')[0]
-    const filename = `v${version}_${configCode}_${timestamp}.json`
-
-    // Ensure the filename is not longer than 24 characters
+    const timestamp = new Date().toISOString()
+    const filename = `v${version}_${timestamp}`
     const truncatedFilename = filename.slice(0, 24)
 
-    // Save the filtered data to a JSON file
-    const outputPath = path.join(process.cwd(), truncatedFilename)
+    const outputPath = path.join(
+      process.cwd(),
+      `public/${truncatedFilename}.json`
+    )
     fs.writeFileSync(outputPath, JSON.stringify(filteredData, null, 2))
     console.log(`Filtered response data saved to: ${outputPath}`)
 
-    // Log a summary of the filtered response
     console.log(
       'Filtered Data preview:',
       JSON.stringify(filteredData.slice(0, 2), null, 2)
@@ -82,5 +70,4 @@ async function uploadFile() {
   }
 }
 
-// Call the function
 uploadFile()
